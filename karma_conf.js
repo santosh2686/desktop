01857d2d42f4de79dc46a34f5ae9b1614610421b
santosh chaudhari <santosh.chaudhari@aexp.com>
2016-05-04 15:33:52 -0700

    Unit testing

--- karma_conf.js
+++ karma_conf.js
@@ -10,13 +10,13 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ['mocha', 'chai', 'sinon', 'chai-sinon'],
 
 
     // list of files / patterns to load in the browser
     files: [
+       
     ],
-
 
     // list of files to exclude
     exclude: [
@@ -26,6 +26,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+        
     },
 
 
