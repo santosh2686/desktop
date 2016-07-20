da215c6dc4aece2c4ae2d026c56686e50e6b7123
santosh2686 <santosh2686@gmail.com>
2016-07-20 16:19:04 -0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -115,7 +115,7 @@ gulp.task('watch',function(){
 });
 
 gulp.task('bower',sequence('bower-copy','bower-inject','bower-minify'));
-gulp.task('build',sequence(['sass','javascript','template'],'inject','bower','watch'));
+gulp.task('build',sequence(['sass','javascript','template'],'inject','bower'));
 
 
 // run server 
