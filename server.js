b9b753e0a472fd5efa307a6472d88bbff20f74dd
santosh2686 <santosh2686@gmail.com>
2016-12-08 17:26:16 -0700

    Update server.js

--- server.js
+++ server.js
@@ -153,3 +153,31 @@ app.route('/api/:databse/:collection/:id')
 var port = process.env.PORT || 9090;
 app.listen(port);
 console.log('Application Running on port : '+port);
+
+
+/*
+//Comvert Excel sheet to JSON.
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+});
+
+
+var node_xj = require("xls-to-json");
+node_xj({
+    input: "task.xlsx",  // input xls
+    output: "output.json" // output json
+}, function(err, result) {
+    if(err) {
+        console.error(err);
+    } else {
+        console.log(result);
+    }
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+});
+/*
