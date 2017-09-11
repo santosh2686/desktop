3173b70bc650e5bac62d617275e93245d88e7d41
Santosh Chaudhari <santosh.chaudhari@ge.com>
2017-09-11 14:29:16 -0700

    server js

--- server.js
+++ server.js
@@ -8,10 +8,11 @@ const request = require('request');
 const port = process.env.PORT || 9090;
 const proxyMiddleware = require('http-proxy-middleware');
 
-
+/*
 const HttpsProxyAgent = require('https-proxy-agent');
 const proxy = process.env.https_proxy || process.env.HTTPS_PROXY;
 const agent = new HttpsProxyAgent(proxy);
+*/
 
 
 const session = require('express-session');
@@ -72,7 +73,7 @@ app.use('/v1/**', auth, proxyMiddleware({
   target: 'https://api.mongolab.com',
   changeOrigin: true,
   secure: false,
-  agent: agent,
+  // agent: agent,
   pathRewrite: {
     '^/v1/': '/'
   },
